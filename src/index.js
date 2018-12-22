d6f2a00cc10a664b6bc971a79869b4400e549471
Ewan Dennis <ewandennis@users.noreply.github.com>
2018-12-22 15:24:22 +0000

    Added timing, rebuilt

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ SVG.on(document, 'DOMContentLoaded', () => {
   const height = '100%';
   const svg = SVG('gerhardt').size(width, height);
   const parent = document.getElementById('gerhardt');
+  const t1 = new Date();
   App({ svg, width: parent.clientWidth, height: parent.clientHeight });
+  const t2 = new Date();
+  console.log(`Runtime: ${(t2 - t1) / 1000}sec`);
 });
